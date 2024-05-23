71709528f5bb98b4b137148422d6f8e8080821ab
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-23 15:01:03 +0200

    add Type Model & Edit Type and Project Model HasBelongs/HasMany Commit

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'slug', 'link', 'content', 'cover_image', 'video'];
+
+
+    /**
+     * Get the Type that owns the Project
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
